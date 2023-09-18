c90bf6280ca2453a91eb6cdcd34139be4f01e75e
NicolasAmigo <namigo@fi.uba.ar>
2023-09-18 15:36:06 -0300

    Agregado parser de start server

---
+++ src/start-server.py
@@ -0,0 +1,26 @@
+import argparse
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-v", "--verbose",
+        required=False, help="increase output verbosity", action="store_true"
+    )
+    group.add_argument(
+        "-q", "--quiet",
+        required=False, help="decrease output verbosity", action="store_true"
+    )
+    parser.add_argument(
+        "-H", "--host", dest="ADDR",
+        required=True, help="server IP address", action="store", type=str
+    )
+    parser.add_argument(
+        "-p", "--port", dest="PORT",
+        required=True, help="server port", action="store", type=int
+    )
+    parser.add_argument(
+        "-s", "--storage", dest="FILEPATH",
+        required=True, help="storage dir path", action="store", type=str
+    )
+    return parser.parse_args()
