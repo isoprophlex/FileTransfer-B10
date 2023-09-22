a1ec0b68f2a8e3d449ef0d5e5e4eb51aa7f59ba4
Franco <fcuppari@fi.uba.ar>
2023-09-22 10:01:14 -0300

    Add protocol changes

--- src/Protocol.py
+++ src/Protocol.py
@@ -11,8 +11,35 @@ class Data (Message):
 
     def get_data(self):
         return self.data
+class Start (Message):
+    filename: str
+    filesize: int
+    operation_type: bool
+    def __init__(self, number, type, filename, filesize, operation_type):
+        self.sqn_number = number
+        self.type = type
+        self.filename = filename
+        self.filesize = filesize
+        self.operation_type = operation_type
+
+    def get_filename(self):
+        return self.filename
+    def get_filesize(self):
+        return self.filesize
+    def get_operation_type(self):
+        return self.operation_type
 
 
+class Error(Message):
+    error_type: int
+    def __init__(self, error):
+        self.error_type = error
 
+class ACK(Message):
+    ack: bool
+    def __init__(self, value):
+        self.ack = value
+    def set_ACK(self, value):
+        self.ack = value
 class Protocol:
-    # La idea es, recibir del socket y ver, si es tal tipo, hago un switch y pruebo ese tipo
+        # La idea es, recibir del socket y ver, si es tal tipo, hago un switch y pruebo ese tipo
