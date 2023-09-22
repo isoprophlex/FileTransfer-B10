987c988f66b62ff5dce28c8485a56044a0134060
bravoNicolas <bravo.nicolasf@gmail.com>
2023-09-22 00:47:48 -0300

    client - server example

--- src/client/main.py
+++ src/client/main.py
@@ -1,4 +1,4 @@
-
+from socket import *
 import sys
 
 def main():
@@ -7,8 +7,25 @@ def main():
             print("Bad program call.")
             return -1
 
-        print(f"arg 0 {sys.argv[0]}")
-        #client.run()
+        # print(f"arg 0 {sys.argv[0]}")
+        # client.run()
+        print("client main")
+        serverName = '127.0.0.1'
+        serverPort = 12000
+        serverAddress = (serverName, serverPort)
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        
+        while True:
+            message = input("Input lowercase sentence (or 'q' to quit): ")
+            if message.strip().lower() == 'q':
+                break
+            clientSocket.sendto(message.encode(), serverAddress)
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+            print("serverAddress: ", serverAddress)
+            print(modifiedMessage.decode())
+
+            
+        clientSocket.close()
 
         return 0
 
--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,24 @@
-
+import os
+from socket import *
 import sys
+import threading
+
+def handle_socket_logic():
+    try:
+        serverPort = 12000
+        serverSocket = socket(AF_INET, SOCK_DGRAM)
+        serverSocket.bind(('', serverPort))
+        print("The server is ready to receive")
+
+        while True:
+            message, clientAddress = serverSocket.recvfrom(2048)
+            print("Client Address: ", clientAddress)
+            modifiedMessage = message.decode().upper()
+            serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+            print("Message sent")
+
+    except Exception as err:
+        print(f"Something went wrong and an exception was caught: {str(err)}")
 
 def main():
     try:
@@ -7,8 +26,16 @@ def main():
             print("Bad program call.")
             return -1
 
-        print(f"arg 0 {sys.argv[0]}")
-        #server.run()
+        print("server main")
+
+        socket_thread = threading.Thread(target=handle_socket_logic)
+        socket_thread.daemon = True
+        socket_thread.start()
+
+        while True:
+            user_input = input("")
+            if user_input.strip().lower() == 'q':
+                os._exit(0)
 
         return 0
 
